ea3e0cc8f47b38dbf5e0de9c3ddb9430e04eb799
stevejd88 <stevejd88@gmail.com>
2021-06-24 12:19:17 -0500

    Add onClick and fetch data on button click

--- src/App.js
+++ src/App.js
@@ -8,12 +8,25 @@ function App() {
 
   const onChange = (e) => {
     setUserName(e.target.value);
+    console.log(userName);
+  };
+
+  const onClick = (username) => {
+    fetch(`https://api.github.com/users/${username}/repos`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
   };
 
   return (
     <div className='App'>
       <FormInput onChange={onChange} name='username' value={userName} />
-      <Button type='button' label='Search User' />
+      <Button
+        type='button'
+        label='Search User'
+        onClick={() => onClick(userName)}
+      />
     </div>
   );
 }
--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import "./button.styles.css";
 
-const Button = ({ type, label, disabled }) => {
+const Button = ({ type, label, disabled, onClick }) => {
   return (
-    <button type={type} disabled={disabled}>
+    <button type={type} disabled={disabled} onClick={onClick}>
       {label}{" "}
     </button>
   );
