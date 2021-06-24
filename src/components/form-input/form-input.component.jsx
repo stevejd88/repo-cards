e81d50d807fbdf10d5f0cd022ac6cd65d4272b48
stevejd88 <stevejd88@gmail.com>
2021-06-24 12:00:19 -0500

    first commit

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import "./form-input.styles.css";
+
+const FormInput = ({
+  onChange,
+  onBlur,
+  label,
+  id,
+  name,
+  value,
+  type,
+  placeholder,
+  readOnly,
+  disabled,
+  error,
+  touched,
+  requiredField
+}) => {
+  return (
+    <div className='text-input group'>
+      <label htmlFor={id}>
+        {label}
+        {requiredField && <span className='required-field'>&#42;</span>}
+      </label>
+      <input
+        className='input'
+        id={id}
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        readOnly={readOnly}
+        disabled={disabled}
+        placeholder={placeholder}
+      ></input>
+      {/* {touched.address && value ? (
+        <p className='success'>Answer recorded</p>
+      ) : (
+        <p className='error'>{error.address}</p>
+      )} */}
+    </div>
+  );
+};
+
+export default FormInput;
